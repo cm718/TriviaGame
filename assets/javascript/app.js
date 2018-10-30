05763fd1724240de3ac1497c39e3b80c2968cbdb
cm718 <cm718@icloud.com>
2018-10-29 22:24:35 -0500

    Trivia questions as objects in an array. The loop is for testing.

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+var questions = [
+  {
+    question: "What is Superman's greatest weakness?",
+    answerOne: "Blue cheese",
+    answerTwo: "Kryptonite",
+    answerThree: "Gambling",
+    answerFour: "Lois Lane",
+    correct: "Lois Lane",
+  },
+  {
+    question: "What is Dick Grayson's secret identity?",
+    answerOne: "Martian Manhunter",
+    answerTwo: "Green Arrow",
+    answerThree: "Nightwing",
+    answerFour: "Aquaman",
+    correct: "Nightwing",
+  },
+  {
+    question: "What year did Spider-man make his debut?",
+    answerOne: "1951",
+    answerTwo: "1962",
+    answerThree: "1973",
+    answerFour: "1949",
+    correct: "1962",
+  }
+];
+
+function askEach() {
+  for (i = 0; i < questions.length; i++){
+    console.log(questions[i].question);
+    console.log(`Answer: ${questions[i].correct}`);
+  }
+}
+
+askEach();
